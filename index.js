1a76af7af330f5361db17e4a2e18a758a5af87dd
Anneliese Kappey <anneliesekappey@gmail.com>
2022-10-22 08:33:26 -0300

    lab done minus bonus

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.length) {
+      return this.items[pos]
+  } else throw new Error('OutOfBounds');
+}
 
-  max() {}
+  max() {
+    if (!this.length) {
+      throw new Error('EmptySortedList');
+    } else if (this.length > 0) {
+      let max = this.items[this.items.length - 1]
+      return max
+  }
+}
 
-  min() {}
+  min() {
+    if (!this.length) {
+      throw new Error('EmptySortedList');
+    } else if (this.length > 0) {
+      let min = this.items[0]
+      return min
+    }
+  }
 
   sum() {}
 
   avg() {}
 }
+
+// const sortedList1 = new SortedList(array, array.length());
 
 module.exports = SortedList;
